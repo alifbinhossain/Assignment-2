aed9aab01eec273b41d8ab2eb48de5f20ed09dbe
alifbinhossain <alifahmed47@gmail.com>
2026-05-30 21:49:37 +0600

    feat: add build configuration and update server entry point for deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,8 +8,6 @@ const config = {
   port: Number(process.env.PORT),
   secret: process.env.JWT_SECRET as string,
   secret_expiry: process.env.JWT_SECRET_EXPIRY as any,
-  refresh_secret: process.env.JWT_REFRESH_SECRET as string,
-  refresh_secret_expiry: process.env.JWT_REFRESH_SECRET_EXPIRY as any,
 };
 
 export default config;
